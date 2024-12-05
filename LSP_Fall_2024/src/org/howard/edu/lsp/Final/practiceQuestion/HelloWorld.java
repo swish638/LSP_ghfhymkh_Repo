7e6e6c651b7682a34bfb11ebf613f585ef81a85a
lioan <lioan@DESKTOP-IO3TF4O>
2024-12-05 13:39:05 -0500

    final practice question w/ javadocs and driver java file (also a comment)

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/Final/practiceQuestion/HelloWorld.java
@@ -0,0 +1,32 @@
+package org.howard.edu.lsp.Final.practiceQuestion;
+
+//EXAMPLE JAVADOCS:
+
+/**
+ * This class represents a set of integers and provides methods for set operations
+ * such as adding elements, removing elements, and checking if a set contains a certain element.
+ * This class also provides methods to perform union, intersection, and difference
+ */
+
+//update 10:12am oct 5th
+
+/**
+ * The HelloWorld class is a simple entry point for a Java application.
+ * This class contains the main method which can be used to execute the program.
+ */
+public class HelloWorld {
+
+    /**
+     * The main method serves as the starting point for the application.
+     * 
+     * @param args command-line arguments passed to the program
+     *             (not used in this example).
+     *             
+     * @return function type void, doesnt return anything
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello, World!");
+    }
+}
+
---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/Final/practiceQuestion/HelloWorldDriver.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.Final.practiceQuestion;
+
+/**
+ * this is the driver code for hello world
+ */
+
+public class HelloWorldDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
